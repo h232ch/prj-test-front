b0dab9c19a307916947b0cc515592871a1d6be28
sehwankim <dnwn7166@gmail.com>
2023-12-03 20:12:43 +0900

    authentication

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,40 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
+import {AuthService} from "./auth.service";
+import {map, take, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return undefined;
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // return this.authService.user.pipe(map(user => {
+    //   // if user is valid return true, not valid return false.
+    //   return !!user;
+    // }), tap(isAuth => {
+    //   if (!isAuth) {
+    //     this.router.navigate(['/auth']);
+    //   }
+    // }))
+
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
+        // if user is valid return true, not valid return false.
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
+        }
+        // it's more advanced version than above code.
+        return this.router.createUrlTree(['/auth']);
+      })
+    )
   }
 
 }
