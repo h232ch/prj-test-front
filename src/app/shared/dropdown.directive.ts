a634a61a20f9a2a75fcc48dd9685f2bbc02184e6
sehwankim <dnwn7166@gmail.com>
2023-12-17 22:35:26 +0900

    directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,14 +1,17 @@
 import {Directive, HostListener, HostBinding, Renderer2, ElementRef} from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]'
+  selector: '[appDropdown]',
+  exportAs: "appDropdown",
 })
 export class DropdownDirective {
-
+  // HostBinding can bind css
   @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.show') isShown = false;
 
+  // HostListener can use javascript event
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
+    this.isShown = !this.isShown;
   }
-
 }
