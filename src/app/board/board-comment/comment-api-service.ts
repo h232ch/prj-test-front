fb92ae8d361685dc6e80ceeb2bfebc167f664b34
sehwankim <dnwn7166@gmail.com>
2024-01-05 02:23:49 +0900

    Loading spinner

--- src/app/board/board-comment/comment-api-service.ts
+++ src/app/board/board-comment/comment-api-service.ts
@@ -9,66 +9,67 @@ import {BoardApiService} from "../board-api-service";
 
 @Injectable()
 export class CommentApiService extends ApiClientService<Comment> {
-    // Error variable
-    comment: Subject<Comment> = new Subject<Comment>();
+  // Error variable
+  comment: Subject<Comment> = new Subject<Comment>();
 
-    constructor(
-        private httpClient: HttpClient,
-        private router: Router,
-        private boardApiService: BoardApiService,
-    ) {
-        super(httpClient, 'http://54.180.86.155:8000/api/comments/');
-    }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private boardApiService: BoardApiService,
+  ) {
+    super(httpClient, 'http://54.180.86.155:8000/api/comments/');
+  }
 
-    create(item: Comment, id?: number, childCommentOption?: boolean, parentId?: number): void {
-        let targetUrl = this.apiUrl;
-        if (childCommentOption) {
-            targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
-        }
-        this.httpClient.post<Comment>(`${targetUrl}`, item).subscribe(res => {
-            this.boardApiService.getById(id);
-        }, errorMessage => {
-            this.boardApiService.error.next(errorMessage.error);
-        })
+  create(item: Comment, id?: number, childCommentOption?: boolean, parentId?: number): void {
+    let targetUrl = this.apiUrl;
+    if (childCommentOption) {
+      targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
     }
+    this.httpClient.post<Comment>(`${targetUrl}`, item).subscribe(res => {
+      this.boardApiService.getById(id);
+    }, errorMessage => {
+      this.boardApiService.error.next(errorMessage.error);
+    })
+  }
 
-    delete(id: number, boardId?: number, childCommentOption?: boolean): void {
-        let targetUrl = this.apiUrl;
-        if (childCommentOption) {
-            targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
-        }
-        this.httpClient.delete(`${this.apiUrl}${id}`).subscribe(res => {
-            this.boardApiService.getById(boardId);
-        }, errorMessage => {
-            this.boardApiService.error.next(errorMessage.error);
-        })
+  delete(id: number, boardId?: number, childCommentOption?: boolean): void {
+    console.log('test')
+    let targetUrl = this.apiUrl;
+    if (childCommentOption) {
+      targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
     }
+    this.httpClient.delete(`${targetUrl}${id}`).subscribe(res => {
+      this.boardApiService.getById(boardId);
+    }, errorMessage => {
+      this.boardApiService.error.next(errorMessage.error);
+    })
+  }
 
-    getAll(): void {
-    }
+  getAll(): void {
+  }
 
-    getById(id: number, childCommentOption?: boolean): void {
-        let targetUrl = this.apiUrl;
-        if (childCommentOption) {
-            targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
-        }
-        this.httpClient.get<Comment>(`${targetUrl}${id}`).subscribe(res => {
-            this.comment.next(res);
-        }, errorMessage => {
-            this.boardApiService.error.next(errorMessage.error);
-        });
+  getById(id: number, childCommentOption?: boolean): void {
+    let targetUrl = this.apiUrl;
+    if (childCommentOption) {
+      targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
     }
+    this.httpClient.get<Comment>(`${targetUrl}${id}`).subscribe(res => {
+      this.comment.next(res);
+    }, errorMessage => {
+      this.boardApiService.error.next(errorMessage.error);
+    });
+  }
 
-    update(id: number, item: Comment, boardId: number, childCommentOption?:boolean): void {
-        let targetUrl = this.apiUrl;
-        if (childCommentOption) {
-            targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
-        }
-        this.httpClient.put<Comment>(`${targetUrl}${id}/`, item).subscribe(res => {
-            this.boardApiService.getById(boardId);
-        }, errorMessage => {
-            this.boardApiService.error.next(errorMessage.error);
-        })
+  update(id: number, item: Comment, boardId: number, childCommentOption?: boolean): void {
+    let targetUrl = this.apiUrl;
+    if (childCommentOption) {
+      targetUrl = 'http://54.180.86.155:8000/api/child_comments/'
     }
+    this.httpClient.put<Comment>(`${targetUrl}${id}/`, item).subscribe(res => {
+      this.boardApiService.getById(boardId);
+    }, errorMessage => {
+      this.boardApiService.error.next(errorMessage.error);
+    })
+  }
 
 }
