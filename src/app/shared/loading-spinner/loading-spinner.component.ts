ffa1c7ff88cdf414121fb8637aa914517e2772aa
sehwankim <dnwn7166@gmail.com>
2024-01-05 05:31:39 +0900

    Loading spinner

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loading-spinner',
   template:
-    '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>',
+
+    '<div class="backdrop">' +
+    '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>' +
+    '</div>',
   styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {}
